99d711bb1fd942bddd4c22bae9e3f48910374a71
P Sarath Kumaran <161850376+p-sarath-kumaran@users.noreply.github.com>
2024-06-21 18:45:11 +0530

    Add files via upload

---
+++ E-Commerce/com.js
@@ -0,0 +1,27 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll" , function(){
+    header.classList.toggle ("sticky" , window.scrollY > 0);
+});
+
+let menu = document.querySelector("#menu-icon");
+let nav = document.querySelector(".nav");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle("open");
+};
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    nav.classList.remove("open");
+};
+
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.home-text',{delay: 200 , origin: 'top'});
+sr.reveal('.home-img',{delay: 350 , origin: 'top'});
+sr.reveal('.feature , .product, .cta-content , .contact',{delay: 200 , origin: 'top'});
